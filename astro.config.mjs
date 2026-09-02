ca519109898a3426ce9b98c445f90a5f7f27549f
Maddie <lbplover19.ml@gmail.com>
2026-09-02 11:27:03 -0400

    Added robots.txt and sitemap.xml (w placeholder URL for now)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://full-portfolio.vercel.app', // Update before going live!
+    integrations: [sitemap()],
     fonts: [{
         provider: fontProviders.fontsource(),
         name: "Caveat",
