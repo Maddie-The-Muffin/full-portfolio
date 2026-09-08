b106a9edb8f180831d12398bdd15438c8f664d10
Maddie <lbplover19.ml@gmail.com>
2026-09-08 16:12:34 -0400

    Updated domains across the repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://full-portfolio.vercel.app', // Update before going live!
+    site: 'https://maddietavares.dev',
     integrations: [sitemap()],
     fonts: [{
         provider: fontProviders.fontsource(),
